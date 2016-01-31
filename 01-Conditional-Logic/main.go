3295a9ee8bfb54ba646c1413eb2dc1ebc1325132
bpadilla11 <bpadilla11@mail.fresnostate.edu>
2016-01-31 12:47:30 -0800

    Added comments and changed to a relevant test user.

--- 01-Conditional-Logic/main.go
+++ 01-Conditional-Logic/main.go
@@ -1,3 +1,9 @@
+/*
+	Brian Padilla
+	1/30/15
+	Go template use with a reference to Paradise City by Guns 'n Roses.
+*/
+
 package main
 
 import (
@@ -6,32 +12,36 @@ import (
 	"text/template"
 )
 
+// Person struct: takes a Name as a string and Age as an int
 type person struct {
 	Name string
 	Age  int
 }
 
+// Paradise City struct: takes from the person struct and TakeMeHome as a bool
 type paradiseCity struct {
 	person
 	TakeMeHome bool
 }
 
 func main() {
-	p1 := paradiseCity {
-		person: person {
-			Name: "Ben Dover",
-			Age: 69,
+	p1 := paradiseCity { // Create a Paradise City
+		person: person { // Create a person
+			Name: "Slash", // Best guitar player of all time!
+			Age: 50, // Age of Slash as of 1/30/16
 		},
-		TakeMeHome: true,
+		TakeMeHome: true, // Oh won't you please take me home!
 	}
 
+	// Parse the templ.gohtml file
 	tpl, err := template.ParseFiles("templ.gohtml")
-	if err != nil {
+	if err != nil { // If templ.gohtml does not exist, give user a error
 		log.Fatalln(err)
 	}
 
+	// Execute the webpage
 	err = tpl.Execute(os.Stdout, p1)
-	if err != nil {
+	if err != nil { // If p1 does not exist, give user a error
 		log.Fatalln(err)
 	}
 
