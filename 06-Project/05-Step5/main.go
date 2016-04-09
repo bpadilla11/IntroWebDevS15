0e4d5204876ab8348d39f42e0eb07c167ab479e4
bpadilla11 <bpadilla11@mail.fresnostate.edu>
2016-04-09 14:49:31 -0700

    Forgot to change the date on step 5 of the project

--- 06-Project/05-Step5/main.go
+++ 06-Project/05-Step5/main.go
@@ -1,5 +1,5 @@
 /* Brian Padilla
- * 3/21/16
+ * 4/9/16
  * PROJECT STEP 5 - continuing to build our application, integrate HMAC into our
  * application to ensure that nobody tampers with the cookie.
  */
